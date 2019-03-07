ff78b02ead957625fb75854f9ab89d579866b0f2
Dameon Sumpton <dameon.sumpton@cpoms.co.uk>
2019-03-07 08:43:57 +0000

    updated filter/filter_all names

--- lib/simple_searchable.rb
+++ lib/simple_searchable.rb
@@ -6,23 +6,23 @@ module SimpleSearchable
       self.singleton_class.send(:define_method, :search) do |opts|
         return all if opts.nil?
 
-        filter_all(method_names, opts)
+        search_filter_all(method_names, opts)
       end
 
       self.send(:extend, FilterMethods)
     end
 
     module FilterMethods
-      def filter(name, arg)
+      def search_filter(name, arg)
         arg.present? ? send(name, arg) : all
       end
 
-      def filter_all(names, opts)
+      def search_filter_all(names, opts)
         result = self.all
         names.each do |n|
           unless opts[n].blank?
             opts[n].reject!(&:blank?) if opts[n].is_a?(Array)
-            result = result.filter(n, opts[n])
+            result = result.search_filter(n, opts[n])
           end
         end
         result
